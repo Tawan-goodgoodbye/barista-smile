d1cac5f4e9f7c29bc3e2a4344a57f92e70ffc14c
Tawan-goodgoodbye <sawnyanlintun26@gmail.com>
2025-12-05 18:23:57 +0700

    Create ComparisonSlider.tsx

---
+++ components/ComparisonSlider.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ComparisonSlider = () => {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center', color: 'white' }}>
+      <p>Comparison Slider Placeholder</p>
+      {/* You can add the real slider code later */}
+    </div>
+  );
+};
+
+export default ComparisonSlider;
